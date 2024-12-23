31dab03eea8dfdf853d17a80d5b6daf353015267
Anonda25 <anondabiswas25@gmail.com>
2024-12-23 21:24:43 +0600

    added the cookies token

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
 //meidlewere
-app.use(cors())
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
+
+
+const verifyToken =(req, res, next)=>{
+const token = req?.cookies?.token
+    if(!token){
+        return res.status(401).send({message : 'unAuthorize access'})
+    }
+    jwt.verify(token, process.env.JWT_SECRET,  (err, decoded)=> {
+        if(err){
+            return res.status(401).send({ message: 'unAuthorize access' })
+        }
+        next()
+    });
+    
+    
+}
 
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.ls3lx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,7 +53,25 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
 
-        // get the servise
+
+        // get  apis token 
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '10h' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+
+                .send({ success: true })
+        })
+
+
+        
+
+
+        // get the servise/serch
         app.get('/services', async (req, res) => {
             const { sherchprams } = req.query;
             let option = {}
@@ -91,10 +132,9 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/serviceStatus/:email', async (req, res) => {
+        app.get('/serviceStatus/:email', verifyToken,  async (req, res) => {
             const isBuyer = req.query.buyer;
             const email = req.params.email;
-            console.log(isBuyer);
             let query = {}
             if (isBuyer) {
                 query.buyer = email
